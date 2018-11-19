98a6909d00f4ec687b2e5f019e0156d8bc1e419e
arredo2018 <44814018+arredo2018@users.noreply.github.com>
2018-11-19 10:39:10 -0700

    Add files via upload

---
+++ mathops01.py
@@ -0,0 +1,26 @@
+
+
+a = 25
+b = 10
+c = 7
+
+print("read the results of :,a," + ", b, " =" ,a + b)
+print("read the results of :,a," - ", b, " =" ,a - b)
+print("read the results of :,a," * ", b, " =" ,a * b)
+print("read the results of :,a," / ", b, " =" ,a / b)
+
+
+# Intenger division //
+
+print("read the results of :,a," // ", b, " =" ,a // b)
+print("read the results of :,a," // ", b, " =" ,a // c)
+
+
+# Modulus of an integer division
+
+print("read the results of :,a," % ", b, " =" ,a % c)
+print("read the results of :,c," % ", b, " =" ,c % b)
+
+# power of a base raised to an exponent
+
+print(" find 2 raised at the 5th power ")
