f052afc6eb751c857a4151a57f651984a22015de
arredo2018 <44814018+arredo2018@users.noreply.github.com>
2019-01-08 10:36:02 -0700

    Add files via upload

---
+++ myforloop01.py
@@ -0,0 +1,23 @@
+#print()
+
+#for i in range(1, 10):
+#    print(i)
+
+#for j in range(1, 10):
+#    print(j, end='')
+#print()
+#for k in range(1, 10):
+#    print(k, '  ', end='')
+
+#print()
+#for w in range(1, 101):
+#    print('{0:3d}'.format(w))
+
+#for u in range(100, 10001,500):
+#    print('{0:5d}'.format(u))
+
+
+for row in range(1, 10):
+    for col in range(1,5):
+        print(row, col,'  ', end=row)
+        print(row)
