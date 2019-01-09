4cc936f985fbd8f8a7c945add42f462110928800
arredo2018 <44814018+arredo2018@users.noreply.github.com>
2019-01-09 11:08:30 -0700

    Add files via upload

---
+++ whileloops.py
@@ -0,0 +1,40 @@
+'''
+1) swap two variables
+2) fibonacci sequence
+'''
+#a = 5
+#b = 10
+#print('the values for a =', a,'and the value for b =',b)
+
+# create a temporary var , temp
+
+#temp = a # place the value from a into temp
+#a = b    # assign to var a the content of b
+#b = temp # assign b the temp value
+
+#print('the value for a =',a,'and the value for b =',b)
+
+# while loops
+
+
+#x = 1
+#while x < 10:
+   # print(x)
+   # x = x + 1
+
+#cont = 'c'
+#while cont == 'c':
+   #print(" press c if u want more, otherwise press any key")
+
+   # cont = input('enter c if u want more')
+
+
+x = 1
+y = 1
+cont = 'c'
+while cont == 'c':
+    print('a new fibon number is ',x +y)
+    temp = x
+    x = y
+    y = y + temp
+    cont = input("if u want more fibon nums press c, else press any")
