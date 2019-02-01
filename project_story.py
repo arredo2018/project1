495226fcc09ed3182e56c3a690dac67645b2cedb
arredo2018 <44814018+arredo2018@users.noreply.github.com>
2019-02-01 10:38:21 -0700

    Add files via upload

--- project_story.py
+++ project_story.py
@@ -2,6 +2,8 @@ from storytext import text
 from sixtolib import translate
 
 
-mydicttionary = {'Spider' : input('Replacement for spider')}
+mydicttionary = {'Spider' : input('Replacement for spider'), 'Mr' : input('replacement for mr'),
+                 'dog' : input('replacement for dog'), 'bald' : input('replacement for bald'),
+                 'he' : input('replacemnet for he'), 'his' : input('replacement for him')}
 new_text = translate(text, mydicttionary).split('.')
 print(new_text)
